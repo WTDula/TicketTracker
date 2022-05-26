8c4a240ecd609427f5ba317f7ce8e5f59b8cfc19
WTDula <wtdula@gmail.com>
2022-05-26 10:21:56 -0500

    set up AdminTable component with ideas on what to accomplish with it

--- frontend/src/components/AdminTable/AdminTable.jsx
+++ frontend/src/components/AdminTable/AdminTable.jsx
@@ -1,11 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const AdminTable = (props) => {
+
+    const [tickets, setTickets] = useState([])
+
+    useEffect(() => {
+        setTickets(props.tickets)
+    }, [props.tickets])
+
+    //display a table showing each engineer and what tickets are assigned to them then
+    //show unassigned tickets using arrays in arrays
+
     return ( 
-        <div>
-            
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Engineer</th>
+                    <th>Status</th>
+                    <th>Description</th>
+                    <th>Assigned To</th>
+                    <th>Posted By</th>
+                    <th>Deadline</th>
+                    <th>Completed?</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tickets.map((ticket, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{ticket.name}</td>
+                            <td>{ticket.status}</td>
+                            <td>{ticket.description}</td>
+                            <td>{ticket.assigned_to.last_name}, {ticket.assigned_to.first_name}</td>
+                            <td>{ticket.posted_by.last_name}, {ticket.posted_by.first_name}</td>
+                            <td>{ticket.deadline}</td>
+                            <td>{ticket.is_finished ? "Yes" : "No"}</td>
+                            
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>  
      );
 }
  
