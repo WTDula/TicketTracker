5d88d9f16a754552587c57bc2b8f66c2af505710
WTDula <wtdula@gmail.com>
2022-05-30 15:45:09 -0500

    formatted navbar component using Material UI (MUI), changed app title in index.html, removed navbar.css (styling done w/ MUI)

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,28 +2,64 @@ import React from "react";
 import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
-import "./NavBar.css";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
-  const { logoutUser, user } = useContext(AuthContext);
+  const { logoutUser, user } = useContext(AuthContext)
   const navigate = useNavigate();
   return (
-    <div className="navBar">
-      <ul>
-        <li className="brand">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>TicketTracker</b>
-          </Link>
-        </li>
-        <li>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+              <b>TicketTracker</b>
+            </Link>
+          </Typography>
           {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
-        </li>
-      </ul>
-    </div>
+            <Button 
+              style={{background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+              border: 0,
+              borderRadius: 3,
+              boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+              color: 'white',
+              height: 48,
+              padding: '0 30px'}}
+              onClick={logoutUser}>
+                Logout
+            </Button>) : (
+            <Button 
+              style={{background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+              border: 0,
+              borderRadius: 3,
+              boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+              color: 'white',
+              height: 48,
+              padding: '0 30px'}}
+              onClick={() => navigate("/login")}>
+                Login
+            </Button>
+            )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+    // <div className="navBar">
+    //   <ul>
+    //     <li>
+    //       {user ? (
+    //         <button onClick={logoutUser}>Logout</button>
+    //       ) : (
+    //         <button onClick={() => navigate("/login")}>Login</button>
+    //       )}
+    //     </li>
+    //   </ul>
+    // </div>
   );
 };
 
